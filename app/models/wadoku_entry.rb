d8f0c46304d3c51b6c0f120fc22ca5109c0f3834
Roger Braun <roger.braun@student.uni-tuebingen.de>
2010-10-30 17:32:50 +0200

    Revert "small fixes"
    This reverts commit cbf212cff8f64d7c573432f5c39c6996b8a18016.

--- app/models/wadoku_entry.rb
+++ app/models/wadoku_entry.rb
@@ -1,8 +1,4 @@
 class WadokuEntry < ActiveRecord::Base
 
   has_one :kimura_entry
-
-  def kanji_and_translation
-    "#{kanji}: #{translation[0..60]}#{translation.size >= 60 ? '...' :''}"
-  end
 end
